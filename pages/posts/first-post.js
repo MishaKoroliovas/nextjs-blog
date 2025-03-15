94491cb1631931b1c24dceae533ff8883d561b11
Misha Koroliov <koroliov1985@gmail.com>
2025-03-15 12:28:51 +0000

    Added basic css to components

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,13 @@
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function(){
-    return <h1> 
-        <Link href="/">Back to home page</Link>
-        </h1>
+    return (
+    <Layout>
+    <h1>First Post</h1>
+    <h2><Link href="/">Back to home page</Link></h2>
+
+        </Layout>
+        
+    );
 }
